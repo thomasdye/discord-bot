5f4f19b5ff4ce4b497af0b7bfacc19d90cba70ff
Casualty <12024874+thomasdye@users.noreply.github.com>
2023-04-12 11:21:25 -0400

    moves patch notes url to const and console logs when sending

--- commands/overwatchPatchNotes.js
+++ commands/overwatchPatchNotes.js
@@ -1,10 +1,12 @@
 const { SlashCommandBuilder } = require('discord.js');
+const overwatchUrl = 'https://overwatch.blizzard.com/en-us/news/patch-notes/'
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('overwatch-patch-notes')
 		.setDescription('Posts a link to the latest Overwatch patch notes.'),
 	async execute(interaction) {
-		await interaction.reply('https://overwatch.blizzard.com/en-us/news/patch-notes/');
+		console.log('sending overwatch patch notes')
+		await interaction.reply(overwatchUrl);
 	},
 };

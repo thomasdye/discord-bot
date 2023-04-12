cf18465d53c72b491ae672f1facebf9264082963
Casualty <12024874+thomasdye@users.noreply.github.com>
2023-04-12 12:35:49 -0400

    fixes naming convention to be uniform

--- commands/overwatchPatchNotes.js
+++ commands/overwatchPatchNotes.js
@@ -3,7 +3,7 @@ const overwatchUrl = 'https://overwatch.blizzard.com/en-us/news/patch-notes/'
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('overwatch-patch-notes')
+		.setName('overwatchPatchNotes')
 		.setDescription('Posts a link to the latest Overwatch patch notes.'),
 	async execute(interaction) {
 		console.log('sending overwatch patch notes')

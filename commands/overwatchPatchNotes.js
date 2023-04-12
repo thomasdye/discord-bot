73a6344933c70bb82106985c267c108476b98469
Casualty <12024874+thomasdye@users.noreply.github.com>
2023-04-12 12:38:29 -0400

    Update overwatchPatchNotes.js

--- commands/overwatchPatchNotes.js
+++ commands/overwatchPatchNotes.js
@@ -3,7 +3,7 @@ const overwatchUrl = 'https://overwatch.blizzard.com/en-us/news/patch-notes/'
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('overwatchPatchNotes')
+		.setName('overwatch')
 		.setDescription('Posts a link to the latest Overwatch patch notes.'),
 	async execute(interaction) {
 		console.log('sending overwatch patch notes')

bcdbb627450ca97205f7c847d5751c5ca508a887
Thomas Dye <12024874+thomasdye@users.noreply.github.com>
2023-04-12 11:02:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
+const token = process.env.TOKEN;
 
 // Events
 const eventsPath = path.join(__dirname, 'events');

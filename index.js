ace2d12267d2d1e550307ed14bd41a367b8b4d01
Casualty <12024874+thomasdye@users.noreply.github.com>
2023-04-12 23:09:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -49,9 +49,16 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 
-const { Client, Intents } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+	],
+});
 
 let rawdata = fs.readFileSync('config.json');
 let config = JSON.parse(rawdata);
